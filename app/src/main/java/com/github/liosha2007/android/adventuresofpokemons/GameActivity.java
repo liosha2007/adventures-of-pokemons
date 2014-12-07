a4a2f7a4d3e7feaa8877d8c92354aa50e13440e5
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2014-12-07 14:45:52 +0200

    Added libraries AndEngine, Box2D, TMXTiledMap

---
+++ app/src/main/java/com/github/liosha2007/android/adventuresofpokemons/GameActivity.java
@@ -0,0 +1,31 @@
+package com.github.liosha2007.android.adventuresofpokemons;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.IGameInterface;
+import org.andengine.ui.activity.BaseGameActivity;
+
+/**
+ * Created by liosha on 07.12.2014.
+ */
+public class GameActivity extends BaseGameActivity {
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        return null;
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+
+    }
+}
